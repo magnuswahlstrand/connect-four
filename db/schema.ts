9a77d6a03bf13417877fc28abab29d5e38e2ab55
magnus <magnus.wahlstrand@gmail.com>
2023-08-02 09:53:41 +0200

    wip

---
+++ db/schema.ts
@@ -0,0 +1,25 @@
+import {boolean, integer, jsonb, pgTable, text, timestamp} from "drizzle-orm/pg-core";
+import {Game} from "@/lib/types";
+
+
+export const spaces = pgTable('space', {
+    key: text("key").primaryKey(),
+    version: integer("version").notNull(),
+})
+
+export const replicacheClients = pgTable('replicache_client', {
+    id: text("id").primaryKey(),
+    last_mutation_id: integer("last_mutation_id").notNull()
+})
+
+
+export const entry = pgTable('entry', {
+    key: text("key").primaryKey(),
+    spaceid: text("spaceid").notNull().references(() => spaces.key),
+    value: jsonb("value").$type<Game>().notNull(),
+    deleted: boolean("deleted").notNull(),
+    version: integer("version").notNull(),
+    lastmodified: timestamp("version", {precision: 6}).notNull(),
+});
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import {Inter} from 'next/font/google'
-import {useReducer} from "react";
 import {BoardColumn} from "@/components/Board/Column";
 import {Board, emptyBoard, isGameOver} from "@/lib/board";
+import {Replicache, WriteTransaction} from "replicache";
+import {Game, pullGameValidation} from "@/lib/types";
+import {useSubscribe} from "replicache-react";
 
 const inter = Inter({subsets: ['latin']})
 
+const REPLICACHE_LICENSE_KEY = process.env.NEXT_PUBLIC_REPLICACHE_LICENSE_KEY ?? ""
 
 const newGame = () => {
     return {
@@ -74,17 +77,12 @@ const gameReducer = (state: State, action: Action) => {
                 return state
             }
 
-            const newBoard = state.board.map((col, index) => {
-                if (index === x) {
-                    return [...col.slice(0, y), state.currentPlayer, ...col.slice(y + 1)]
-                } else {
-                    return col
-                }
-            })
+            const copy: number[][] = state.board.map((row: number[]) => Array.from(row));
+            copy[x][y] = state.currentPlayer;
 
-            const isOver = isGameOver(newBoard, x, y, state.currentPlayer)
+            const isOver = isGameOver(copy, x, y, state.currentPlayer)
             return {
-                ...state, board: newBoard,
+                ...state, board: copy,
                 winner: isOver ? state.currentPlayer : null,
                 currentPlayer: state.currentPlayer === 1 ? 2 : 1
             }
@@ -94,15 +92,72 @@ const gameReducer = (state: State, action: Action) => {
     }
 };
 
+type GameUpdate = {
+    gameId: string,
+    game: Game,
+    column: number
+}
+
+const placeMarker = async (tx: WriteTransaction, {gameId, game, column}: GameUpdate) => {
+
+    if (game.winner) {
+        console.log('game is over')
+        return
+    }
+
+    const x = column
+    const y = game.board[x].lastIndexOf(0)
+    if (y === -1) {
+        console.log('invalid move')
+        return
+    }
+
+    const copy: number[][] = game.board.map((row: number[]) => Array.from(row));
+    copy[x][y] = game.currentPlayer;
+
+    const isOver = isGameOver(copy, x, y, game.currentPlayer)
+
+    const updatedGame = {
+        currentPlayer: game.currentPlayer === 1 ? 2 : 1,
+        board: copy,
+        winner: isOver ? game.currentPlayer : null,
+    }
+    await tx.put(`game/${gameId}`, updatedGame);
+    console.log('message put')
+}
+
+const mutators = {
+    placeMarker
+}
+
+const r = process.browser
+    ? new Replicache({
+        name: "MAGNUS",
+        licenseKey: REPLICACHE_LICENSE_KEY,
+        pushURL: '/api/replicache-push',
+        pullURL: '/api/replicache-pull',
+        mutators,
+    }) : null
+
 export default function Home() {
-    const [state, dispatch] = useReducer(gameReducer, newGame());
+    const games = useSubscribe(
+        r,
+        async tx => {
+            // TODO: Remove game hardcoding
+            const list = await tx.scan({prefix: 'game/qpdgkvpb9ao'}).entries().toArray()
+            return pullGameValidation.parse(list)
+        },
+        [],
+    );
 
-    // Destructure state for ease of use
-    const {board, currentPlayer, winner} = state;
+    if (games.length === 0) {
+        return <div>loading...</div>
+    }
 
+    const [gameId, game] = games[0];
 
-    const handleClick = (column: number) => {
-        dispatch({type: 'PLACE_MARKER', column});
+    const handleClick = async (column: number) => {
+        await r?.mutate.placeMarker({gameId: gameId.substring(5), game, column})
     };
 
     return (
@@ -110,7 +165,9 @@ export default function Home() {
             <div className={"flex flex-col items-center justify-center h-screen cursor-pointer"}>
                 <div className="text-2xl font-bold mb-2">Why don&apos;t you</div>
                 <div className="text-4xl text-red-600 font-semibold">Connect four?</div>
-                <GameBoard board={board} onClick={handleClick} currentPlayer={currentPlayer} winner={winner}/>
+                <GameBoard board={game.board}
+                           onClick={handleClick}
+                           currentPlayer={game.currentPlayer} winner={game.winner}/>
             </div>
         </>
     )
